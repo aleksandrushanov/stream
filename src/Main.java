3157cfd9b27946795f3b2bc4f3598dac08fc96fd
Саша <kresh_sasha@icloud.com>
2023-03-22 13:41:13 +0700

    Stream

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
+
+    public static <T> void findMinMax(
+            Stream<? extends T> stream,
+            Comparator<? super T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer
+    ) {
+        List<? extends T> list = stream.sorted(order).collect(Collectors.toList());
+        minMaxConsumer.accept(list.get(0), list.get(list.size()-1));
+    }
     public static void main(String[] args) {
 
+
+        findMinMax(Stream.of(12,55,632,-5,198,502),Integer::compareTo,(min,max)-> System.out.println("Min: "+min + ", Max: " + max));
+
+        evenNumbers(List.of(1,2,3,4,5,6,7,8,9,10,11,12));
+
+    }
+
+    public static void evenNumbers(List<Integer> list) {
+        long count = list.stream().filter(i -> i % 2 == 0).count();
+        System.out.println("Even Numbers: " + count);
     }
 }
